7b5fdc60c3bfcdccc754dce319160694fff6b558
Nishant <me@nishantjha.org>
2024-08-02 15:11:58 -0700

    Hacky state updates in streamlit
    [wip]
    
    working state updates

--- start.py
+++ start.py
@@ -1,121 +1,189 @@
 import streamlit as st
-import pandas as pd
-from pandas.api.types import (
-    is_categorical_dtype,
-    is_datetime64_any_dtype,
-    is_numeric_dtype,
-    is_object_dtype,
-)
-import numpy as np
 from typing import Any, TypedDict, List, Union, Literal
+import streamlit_platemap
 
-import molecule3dplot
-
-st.set_page_config(
-    layout="wide",
-)
-
-
-class FilterColumn(TypedDict):
-    key: str
-    label: str
-    values: List[str]
-    filter_type: Union[Literal["number"], Literal["text"]]
+mock_data = {
+    "wells": 96,
+    "colAnnotations": [
+        {
+            "cols": [0, 1, 2, 3],
+            "label": "Patient 1",
+            "id": "Patient 1",
+            "color": "blue",
+        },
+        {
+            "cols": [4, 5, 6, 7],
+            "label": "Patient 2",
+            "id": "Patient 2",
+            "color": "green",
+        },
+        {
+            "cols": [8, 9, 10, 11],
+            "label": "Patient 3",
+            "id": "Patient 3",
+            "color": "fuchsia",
+        },
+    ],
+    "rowAnnotations": [
+        {
+            "rows": [0, 1],
+            "label": "Antibody 1 μmol/L",
+            "id": "Antibody 1 μmol/L",
+            "color": "cyan",
+        },
+        {
+            "rows": [3, 4],
+            "label": "Antibody 2 μmol/L",
+            "id": "Row Annotation 1",
+            "color": "yellow",
+        },
+        {
+            "rows": [6, 7],
+            "label": "Antibody 3 μmol/L",
+            "id": "Row Annotation 2",
+            "color": "red",
+        },
+    ],
+    "wellAnnotations": [
+        {
+            "id": "Treatment 1",
+            "label": "Treatment 1",
+            "wells": [
+                0,
+                12,
+                36,
+                48,
+                72,
+                84,
+                4,
+                16,
+                40,
+                52,
+                76,
+                88,
+                8,
+                20,
+                44,
+                56,
+                80,
+                92,
+            ],
+            "color": "blue",
+            "metadata": {"foo": "bar"},
+        },
+        {
+            "id": "Treatment 2",
+            "label": "Treatment 2",
+            "wells": [
+                1,
+                13,
+                37,
+                49,
+                73,
+                85,
+                5,
+                17,
+                41,
+                53,
+                77,
+                89,
+                9,
+                21,
+                45,
+                57,
+                81,
+                93,
+            ],
+            "color": "red",
+            "metadata": {"foo": "bar"},
+        },
+        {
+            "id": "Postive Control",
+            "label": "Postive Control",
+            "wells": [
+                2,
+                14,
+                38,
+                50,
+                74,
+                86,
+                6,
+                18,
+                42,
+                54,
+                78,
+                90,
+                10,
+                22,
+                46,
+                58,
+                82,
+                94,
+            ],
+            "color": "green",
+            "metadata": {"foo": "bar"},
+        },
+        {
+            "id": "Negative Control",
+            "label": "Negative Control",
+            "wells": [
+                3,
+                15,
+                39,
+                51,
+                75,
+                87,
+                7,
+                19,
+                43,
+                55,
+                79,
+                91,
+                11,
+                23,
+                47,
+                59,
+                83,
+                95,
+            ],
+            "color": "fuchsia",
+            "metadata": {"foo": "bar"},
+        },
+    ],
+}
 
 
 def main():
-    st.title("Visualize molecules in 3D scatter plots")
-    df = pd.read_csv("dummy_data.csv")
-    df.dropna(inplace=True)
-    data: Any = df.to_dict(orient="records")
+    st.title("Custom Platemap")
+    st.write("This is a custom platemap component")
+    wells = mock_data["wells"]
+    rowAnnotations = mock_data["rowAnnotations"]
+    colAnnotations = mock_data["colAnnotations"]
+    if "wellAnnotations" not in st.session_state:
+        print("initializing wellAnnotations")
+        st.session_state["wellAnnotations"] = mock_data["wellAnnotations"]
+    updated_wellAnnotations = platemap(
+        wells, rowAnnotations, colAnnotations, st.session_state["wellAnnotations"]
+    )
+    have_changes = len(updated_wellAnnotations) > 0
+    if have_changes and updated_wellAnnotations != st.session_state["wellAnnotations"]:
+        st.session_state["wellAnnotations"] = updated_wellAnnotations
+        st.rerun()
 
-    st.header("Select points in 3D scatter plot")
-    selectedPoints3D = []
-    selectedPoints3D = selectedPointPlot(data, selectedPoints3D)
-    selectedRows3D = [data[i] for i in selectedPoints3D]
-    st.table(data=selectedRows3D)
-
-    st.header("Selected points in 2D table")
-    selectedPoints2D = []
-    selectedPoints2D = selectedPointPlot(data, selectedPoints2D, hideZ=True)
-    selectedRows2D = [data[i] for i in selectedPoints2D]
-    st.table(data=selectedRows2D)
-
-    # Combined view
-    combinedPoints = set(selectedPoints3D).union(set(selectedPoints2D))
-
-    # get selectedPoints rows from data
-    combinedRows = [data[i] for i in combinedPoints]
-    st.table(data=combinedRows)
+    st.write("Updated Well Annotations")
+    st.write(st.session_state["wellAnnotations"])
 
 
-def selectedPointPlot(data, selectedPoints, hideZ=False):
-    z = [d["r3"] for d in data]
-    zTitle = "r3"
-    if hideZ:
-        z = None
-        zTitle = None
-    return molecule3dplot.molecule3dplot(
-        selectedPoints=selectedPoints,
-        xTitle="r1",
-        yTitle="r2",
-        zTitle=zTitle,
-        filterColumns=[
-            {
-                "key": "cpd_index",
-                "label": "Compound Index",
-                "values": [d["cpd_index"] for d in data],
-                "filter_type": "text",
-            },
-            {
-                "key": "r1",
-                "label": "r1",
-                "values": [d["r1"] for d in data],
-                "filter_type": "number",
-            },
-            {
-                "key": "r2",
-                "label": "r2",
-                "values": [d["r2"] for d in data],
-                "filter_type": "number",
-            },
-            {
-                "key": "r3",
-                "label": "r3",
-                "values": [d["r3"] for d in data],
-                "filter_type": "number",
-            },
-            {
-                "key": "mw",
-                "label": "Molecular Weight",
-                "values": [d["mw"] for d in data],
-                "filter_type": "number",
-            },
-        ],
-        x=[d["r1"] for d in data],
-        y=[d["r2"] for d in data],
-        z=z,
-        hovertext=[
-            [
-                f"cpd_index: {d['cpd_index']}",
-                f"r1: {d['r1']}",
-                f"r2: {d['r2']}",
-                f"r3: {d['r3']}",
-                f"mw: {d['mw']}",
-            ]
-            for d in data
-        ],
-        smiles=[d["smiles"] for d in data],
-        marker={
-            "size": [8 for _ in data],
-        },
-        categorical=True,
-        colorBy=["foo" if d["r2"] % 2 else "bar" for d in data],
-        legendLabel="Color by foo/bar",
-        overlayClassName="fixed top-0 left-0",
+def platemap(wells, rowAnnotations, colAnnotations, wellAnnotations):
+    return streamlit_platemap.streamlit_platemap(
+        {
+            "wells": wells,
+            "rowAnnotations": rowAnnotations,
+            "colAnnotations": colAnnotations,
+            "wellAnnotations": wellAnnotations,
+        }
     )
 
 
 if __name__ == "__main__":
-
     main()
